62c48c9e66f57caef6e83d6595ee8bbd8c91f503
nathanmerry <46092065+nathanmerry@users.noreply.github.com>
2020-02-05 10:54:32 +0000

    Update partnerdata.js

--- assets/js/partnerdata.js
+++ assets/js/partnerdata.js
@@ -13,7 +13,7 @@ const data = [
       three: "Transfer time: Instant - 3 business days"
     },
     tagline: "none",
-    trohpy: "display: none",
+    trohpy: "display: block",
     id: "CATZKxZesY1ohk3aAjcQ",
     partnerlink: "https://www.awin1.com/cread.php?awinmid=5877&awinaffid=590963&clickref=[tracking_ref]&p=https%3A%2F%2Fwww.worldremit.com%2Fen%2Faccount%2Fsignup",
     encodePartnerLink: function() {
@@ -33,53 +33,12 @@ const data = [
       three: "Transfer time: Same day"
     },
     tagline: "display: block",
-    trohpy: "display: block",
     partnerlink: "https://www.awin1.com/awclick.php?gid=345899&mid=12673&awinaffid=590963&linkid=2241253&clickref=[tracking_ref]",
     id: "zWLVb8d0uVh9dRTFWrL4",
     encodePartnerLink: function() {
       return btoa(this.partnerlink);
     },
     url: "https://www.awin1.com/awclick.php?gid=345899&mid=12673&awinaffid=590963&linkid=2241253&clickref=[tracking_ref]"
-  },
-  {
-    name: "CurrencyFair",
-    logo: "./images/logos/currencyfair.jpg",
-    stars: "./images/stars/5.png",
-    reviewquantity: "3,754",
-    maintagline: "Get 10 free transfers with signup",
-    keyfeatures: {
-      one: "Low fees, competitive exchange rates",
-      two: "Minimum transfer: none",
-      three: "Transfer time: same day"
-    },
-    tagline: "none",
-    trohpy: "display: none",
-    partnerlink: "https://currencyfair.g6ww.net/c/1401712/708874/5170?subId1=[tracking_ref]&sharedid=RMNEK1",
-    id: "ofhSa2xJ0RnVLHdbm1sJ",
-    encodePartnerLink: function() {
-      return btoa(this.partnerlink);
-    },
-    url: "https://currencyfair.g6ww.net/c/1401712/708874/5170?subId1=[tracking_ref]&sharedid=RMNEK1"
-  },
-  {
-    name: "Azimo",
-    logo: "./images/logos/amizo.jpg",
-    stars: "./images/stars/4-half.png",
-    reviewquantity: "34,265",
-    maintagline: "First 2 international transfers are free",
-    keyfeatures: {
-      one: "Minimum transfer: £10",
-      two: "Minimum transfer: None",
-      three: "Transfer time: 30 minutes or less"
-    },
-    tagline: "none",
-    trohpy: "display: none",
-    partnerlink: "https://prf.hn/click/camref:1100l5dqd/pubref:[tracking_ref]/destination:https%3A%2F%2Fazimo.com%2Fen%2Fngk%2Fauth%2Fsign-up%2Femail",
-    id: "uVvcMl5Cg2YyCkhREzt3",
-    encodePartnerLink: function() {
-      return btoa(this.partnerlink);
-    },
-    url: "https://prf.hn/click/camref:1100l5dqd/pubref:[tracking_ref]/destination:https%3A%2F%2Fazimo.com%2Fen%2Fngk%2Fauth%2Fsign-up%2Femail"
   },
   {
     name: "HiFX",
@@ -99,46 +58,6 @@ const data = [
       return btoa(this.partnerlink);
     },
     url: "https://www.awin1.com/awclick.php?gid=345899&mid=12673&awinaffid=590963&linkid=2241253&clickref=[tracking_ref]"
-  },
-  {
-    name: "Paysend",
-    logo: "./images/logos/paysend.jpg",
-    stars: "./images/stars/4-half.png",
-    reviewquantity: "7,233",
-    maintagline: "Special promo code: 'MT'",
-    keyfeatures: {
-      one: "Fixed fees - only £1.00",
-      two: "Minimum transfer: £2.81",
-      three: "Transfer time: Instant - 3 business days"
-    },
-    tagline: "none",
-    trohpy: "display: none",
-    partnerlink: "https://paysend.com/?utm_source=moneytransfers&utm_medium=cpa&utm_campaign=MT&utm_content=[tracking_ref]",
-    id: "bqhjibRAu0EcFcdsyn9E",
-    encodePartnerLink: function() {
-      return btoa(this.partnerlink);
-    },
-    url: "https://paysend.com/?utm_source=moneytransfers&utm_medium=cpa&utm_campaign=MT&utm_content=[tracking_ref]"
-  },
-  {
-    name: "TransferGo",
-    logo: "./images/logos/transfergo.jpg",
-    stars: "./images/stars/5.png",
-    reviewquantity: "13,816",
-    maintagline: "Save up to 90% on international transfers",
-    keyfeatures: {
-      one: "Transaction fee from £0.99",
-      two: "Minimum transfer: None",
-      three: "Transfer time: 30 minutes - 3 days"
-    },
-    url: "www.google.com",
-    tagline: "none",
-    trohpy: "display: none",
-    partnerlink: "https://www.transfergo.com/en",
-    encodePartnerLink: function() {
-      return btoa(this.partnerlink);
-    },
-    url: "https://www.transfergo.com/"
   },
   {
     name: "TransferWise",
@@ -179,25 +98,6 @@ const data = [
       return btoa(this.partnerlink);
     },
     url: "https://www.ofx.com/en-gb/p/gb/money-transfers-com/?PID=12871"
-  },
-  {
-    name: "MoneyGram",
-    logo: "./images/logos/moneygram.jpg",
-    stars: "./images/stars/1-half.png",
-    reviewquantity: "70",
-    maintagline: "Send money to banks around the world",
-    keyfeatures: {
-      one: "Fee: Varies by amount and country",
-      two: "Transfer time: Instant - next day",
-      three: "Daily limit: £6,000"
-    },
-    tagline: "none",
-    trohpy: "display: none",
-    partnerlink: "https://www.moneygram.com/mgo/gb/en/",
-    encodePartnerLink: function() {
-      return btoa(this.partnerlink);
-    },
-    url: "https://www.moneygram.com"
   }
 ];
 
